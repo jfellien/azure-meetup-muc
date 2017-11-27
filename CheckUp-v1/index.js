cac8d6f1e23b01cb148e05817383c9f0d6c737cf
Jan Fellien <jan.fellien@devcrowd.de>
2017-11-27 16:09:35 +0100

    added a http triggered function

---
+++ CheckUp-v1/index.js
@@ -0,0 +1,23 @@
+module.exports = function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    if (req.query.name || (req.body && req.body.name)) {
+
+        const response_message = `"Hello ${ (req.query.name || req.body.name) }`;
+
+        context.bindings.outQueue = {
+            message = response_message
+        }
+
+        context.res = {
+            body: response_message
+        };
+    }
+    else {
+        context.res = {
+            status: 400,
+            body: "Please pass a name on the query string or in the request body"
+        };
+    }
+    context.done();
+};
